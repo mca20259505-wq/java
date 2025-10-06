f3865e835a438e558274246ac09266d5da9e7813
Rehan <mca20259505@gmail.com>
2025-10-06 10:12:35 +0530

    6-10-2025

---
+++ LAB_MANUAL/QS-1/FactorialRecursion.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class FactorialRecursion {
+  
+    static int  fact(int n){
+        if(n == 0){
+            return 1;
+        }
+        return n *= fact(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number :");
+        int num = sc.nextInt();
+        int r = fact(num);
+        System.out.println(r);
+        sc.close();
+    }
+}
