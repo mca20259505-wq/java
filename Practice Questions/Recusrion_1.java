be37249e302c1b59fc5a66bb1de1394819f44060
Rehan <mca20259505@gmail.com>
2025-10-06 09:32:44 +0530

    6-10-2025

---
+++ Practice Questions/Recusrion_1.java
@@ -0,0 +1,12 @@
+public class Recusrion_1 {
+    static void print(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);
+        print(n-1);
+    }
+    public static void main(String[] args) {
+        print(5);
+    }
+}
