a84c3041b164a05b29fdc74800a14235e83f615e
Rehan <mca20259505@gmail.com>
2025-10-07 10:09:12 +0530

    7-10-2025

---
+++ LAB_MANUAL/QS-1/qs_15.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class qs_15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter principal amount :");
+        double p = sc.nextDouble();
+
+        System.out.println("enter rate");
+        double rate = sc.nextDouble();
+
+        System.out.println("Enter duration :");
+        double duration = sc.nextDouble();
+
+        double simple_interest = (p * rate * duration) / 100;
+
+        System.out.println("simple interest :" + simple_interest);
+
+        sc.close();
+    }
+}
