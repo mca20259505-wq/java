e283fd66c38682feabbdffc9d05a65e7f6f8084e
Rehan <mca20259505@gmail.com>
2025-11-10 10:37:15 +0530

    10-11-2025

--- Practice Questions/BasicEnum.java
+++ Practice Questions/BasicEnum.java
@@ -7,5 +7,14 @@ public class BasicEnum {
    public static void main(String[] var0) {
       Level var1 = Level.HIGH;
       System.out.println(var1);
+
+
+
+    //   Looping enum 
+    for(Level var : Level.values())
+    System.out.println(var);
    }
+
+
+
 }
