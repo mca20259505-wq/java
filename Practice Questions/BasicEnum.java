a75b7d09030ac0859ae0fcd3fe099ff248cfead8
Rehan <mca20259505@gmail.com>
2025-11-10 10:34:59 +0530

    10-11-2025

---
+++ Practice Questions/BasicEnum.java
@@ -0,0 +1,11 @@
+enum Level{
+    LOW,
+    MEDIUM,
+    HIGH
+}
+public class BasicEnum {
+   public static void main(String[] var0) {
+      Level var1 = Level.HIGH;
+      System.out.println(var1);
+   }
+}
