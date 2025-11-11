97f6b5f9646b50feacfa2137e9d1c92a6b7fc452
Rehan <mca20259505@gmail.com>
2025-11-11 10:33:52 +0530

    11-11-2025

---
+++ LAB_MANUAL/QS-1/qs_23.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class qs_23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Numbers/String :");
+        String input = sc.nextLine();
+
+        int len = input.length();
+
+        if(len % 2 == 1)
+        {
+            char mid = input.charAt(len/2);
+            System.out.println("middle character :"+mid);
+        }else{
+            char mid1 = input.charAt(len/2-1);
+            char mid2  = input.charAt(len/2);
+            System.out.println("middle character(s) are : " + mid1+mid2);
+        }
+
+        sc.close();
+    }
+}
