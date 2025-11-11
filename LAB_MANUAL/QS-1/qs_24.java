4eaaa6bff2febaf5d4db3c114280409b0e17943d
Rehan <mca20259505@gmail.com>
2025-11-11 11:00:41 +0530

    11-11-2025

--- LAB_MANUAL/QS-1/qs_24.java
+++ LAB_MANUAL/QS-1/qs_24.java
@@ -1,6 +1,60 @@
 import java.util.*;
 public class qs_24 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter number of rows for 2 matrices :");
+        int rows = sc.nextInt();
+
+        System.out.println("enter number of columns for 2 matrices :");
+        int cols = sc.nextInt();
+
+        int[][] matrix1 = new int[rows][cols];
+        int[][] matrix2 = new int[rows][cols];
+
+
+        System.out.println("enter values in matrix-1");
+        for(int i = 0; i<rows;i++){
+            for(int j =0;j<cols;j++)
+            {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("enter values in matrix-2");
+        for(int i = 0; i<rows;i++){
+            for(int j =0;j<cols;j++)
+            {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("\n ===== Matrix-1 =====");
+        for(int i = 0; i< rows; i++)
+        {
+            for(int j = 0; j<cols;j++)
+            {
+                System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n ===== Matrix-2 =====");
+        for(int i = 0; i< rows; i++)
+        {
+            for(int j = 0; j<cols;j++)
+            {
+                System.out.print(matrix2[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("addition of 2 matrices :");
+        for(int i =0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                
+            }
+        }
+        sc.close();
     }
 }
