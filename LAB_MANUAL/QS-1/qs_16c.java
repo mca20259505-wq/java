76e3f31798b71681d214fd081e71266e20173de7
Rehan <mca20259505@gmail.com>
2025-11-12 10:17:19 +0530

    12-11-2025

--- LAB_MANUAL/QS-1/qs_16c.java
+++ LAB_MANUAL/QS-1/qs_16c.java
@@ -1,6 +1,6 @@
 public class qs_16c {
     public static void main(String[] args) {
-        int n = 7; 
+        // int n = 7; 
 
         for (int i = 1; i <= 7; i += 2) {
             for (int j = i; j < 7; j += 2) System.out.print(" ");
