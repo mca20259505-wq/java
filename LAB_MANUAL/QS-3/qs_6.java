c028b14418db637413f44121f0a6888971dcc050
Rehan <mca20259505@gmail.com>
2025-12-09 10:03:44 +0530

    9-12-25

---
+++ LAB_MANUAL/QS-3/qs_6.java
@@ -0,0 +1,25 @@
+interface interface1{
+    public void show();
+}
+interface interface2{
+    public void display();
+}
+
+class A implements interface1,interface2 {
+    public void show()
+    {
+        System.out.println("Hey there!!!");
+    }
+
+    public void display()
+    {
+        System.out.println("weep weep");
+    }
+}
+public class qs_6 {
+    public static void main(String[] args) {
+        A a = new A();
+        a.show();
+        a.display();
+    }
+}
